5dcd95cf0dfcc93a6ebca4ad47da6a37d58ac943
priusz <varro.timi@gmail.com>
2024-06-12 11:19:56 +0200

    add LoginPage, BasePage, HomePage, LoginStep and login feature

---
+++ Reptile-test/src/main/java/com/codecool/pages/HomePage.java
@@ -0,0 +1,19 @@
+package com.codecool.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(linkText = "Logout")
+    private WebElement chooseLogoutButton;
+
+    public String getChooseLogoutButtonText() {
+        return chooseLogoutButton.getText();
+    }
+}
--- Reptile-test/src/test/java/bdd/step_definitions/LoginStep.java
+++ Reptile-test/src/test/java/bdd/step_definitions/LoginStep.java
@@ -1,4 +1,56 @@
 package bdd.step_definitions;
 
+import com.codecool.pages.HomePage;
+import com.codecool.pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginStep {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private HomePage homePage;
+    private final String baseUrl = System.getenv("BASE_URL");
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        System.out.println("fada");
+    }
+
+    @Given("I am a registered user")
+    public void i_am_a_registered_user() {
+        driver.get(baseUrl);
+        System.out.println("Registered user");
+    }
+
+    @When("I try to login with {string} {string}")
+    public void i_try_to_login_with_username_password(String username, String password) {
+        loginPage.handleLogin(username, password);
+        System.out.println("Type in the username and the password");
+    }
+
+    @Then("I see the logout {string}")
+    public void i_see_the_logout_buttonText(String buttonText) {
+        String actualText = homePage.getChooseLogoutButtonText();
+        if (buttonText.equals(actualText)) {
+            System.out.println("Test passed: The actual text matches the expected text.");
+        } else {
+            System.out.println("Test failed: The actual text does not match the expected text.");
+            System.out.println("Expected: " + buttonText);
+            System.out.println("Actual: " + actualText);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
